b2ff4dd9fef1c48921e42d343a7215f1dc65341f
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-11-16 20:39:13 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application") version "8.1.3"
+    id("org.jetbrains.kotlin.android") version "1.9.20"
+}
+
+android {
+    namespace = "epicarchitect.nice"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "epicarchitect.nice"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.github.epicarchitect:epic-adapter:1.0.9")
+}
